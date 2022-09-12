9a995a58d015d9227edc290db5105551191be625
Malvika <shettymalvika11@gmail.com>
2022-09-12 19:55:41 -0400

    first commit

---
+++ API_cardDetails/Controllers/CardDetailController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API_cardDetails.Models;
+
+namespace API_cardDetails.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardDetailController : ControllerBase
+    {
+        private readonly CardDetailContext _context;
+
+        public CardDetailController(CardDetailContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PaymentDetail
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CardDetail>>> GetPaymentDetails()
+        {
+            return await _context.CardDetails.ToListAsync();
+        }
+
+        // GET: api/PaymentDetail/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CardDetail>> GetPaymentDetail(int id)
+        {
+            var paymentDetail = await _context.CardDetails.FindAsync(id);
+
+            if (paymentDetail == null)
+            {
+                return NotFound();
+            }
+
+            return paymentDetail;
+        }
+
+        // PUT: api/PaymentDetail/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPaymentDetail(int id, CardDetail cardDetail)
+        {
+            if (id != cardDetail.PaymentDetailId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cardDetail).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaymentDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PaymentDetail
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<CardDetail>> PostPaymentDetail(CardDetail paymentDetail)
+        {
+            _context.CardDetails.Add(paymentDetail);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPaymentDetail", new { id = paymentDetail.PaymentDetailId }, paymentDetail);
+        }
+
+        // DELETE: api/PaymentDetail/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePaymentDetail(int id)
+        {
+            var paymentDetail = await _context.CardDetails.FindAsync(id);
+            if (paymentDetail == null)
+            {
+                return NotFound();
+            }
+
+            _context.CardDetails.Remove(paymentDetail);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PaymentDetailExists(int id)
+        {
+            return _context.CardDetails.Any(e => e.PaymentDetailId == id);
+        }
+    }
+}
+
---
+++ API_cardDetails/Models/CardDetailContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_cardDetails.Models;
+
+namespace API_cardDetails.Models
+{
+    public class CardDetailContext:DbContext
+    {
+        public CardDetailContext(DbContextOptions<CardDetailContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<CardDetail> CardDetails { get; set; }
+    }
+}
